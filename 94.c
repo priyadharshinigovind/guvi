dda609583a3124bd028635b8c8d2724a806ba003
Priyadharshini Govindarasu <31530607+priyadharshinigovind@users.noreply.github.com>
2018-03-02 15:44:49 +0530

    Create 94.c

---
+++ 94.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+  int a[100],i,n,k;
+  printf("\nEnter limit:\n");
+  scanf("%d",&n);
+  printf("\nEnte rthe index");scanf("%d",&k);
+  printf("\nEnter the elements:\n");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+    if(i==k)
+    {
+      printf("%d",a[i]);
+    }
+  }
+}
